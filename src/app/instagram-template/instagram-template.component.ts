00740e79be6dc611f559abe409165685845327ba
zafiera <zafiera.davids@standardbank.co.za>
2022-08-02 16:47:02 +0200

    instagram template

--- src/app/instagram-template/instagram-template.component.ts
+++ src/app/instagram-template/instagram-template.component.ts
@@ -9,6 +9,10 @@ export class InstagramTemplateComponent implements OnInit {
 
   @Input() date: string | undefined;
   @Input() company: string | undefined;
+  @Input() caption: string | undefined;
+  @Input() companyImage: string | undefined;
+  @Input() mainImage: string | undefined;
+  randomNum = Math.floor((Math.random() * 100000) + 100)
 
 
   constructor() { }
